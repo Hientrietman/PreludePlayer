fbe402faade52ed740c97858a309a1b1e2908026
Hientrietman <vanqui8028@gmail.com>
2024-04-30 16:03:28 +0700

    30/4/2024-Quý-Xử lý kiểm tra quyền ứng dụng và chuyển màn hình qua main

--- app/src/main/java/com/prelude/preludeplayer/MainActivity.java
+++ app/src/main/java/com/prelude/preludeplayer/MainActivity.java
@@ -1,14 +1,29 @@
 package com.prelude.preludeplayer;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PERMISSION_SETTINGS =12 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED){
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            Uri uri = Uri.fromParts("package", getPackageName(),null);
+            intent.setData(uri);
+
+            startActivityForResult(intent, REQUEST_PERMISSION_SETTINGS);
+        }
     }
 }
