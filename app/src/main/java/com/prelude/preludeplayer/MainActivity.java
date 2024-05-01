f2b7c5fd36d19789c98533126ce800da9a65d8b6
Hientrietman <vanqui8028@gmail.com>
2024-05-01 13:37:11 +0700

    Xử lý truy cập vào thư mục chứa vid trên đt và nạp vào RV trên main activity

--- app/src/main/java/com/prelude/preludeplayer/MainActivity.java
+++ app/src/main/java/com/prelude/preludeplayer/MainActivity.java
@@ -1,14 +1,73 @@
 package com.prelude.preludeplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PERMISSION_SETTINGS = 12;
+    private ArrayList<MediaFiles> mediaFiles = new ArrayList<>();
+    private ArrayList<String> allFolderList = new ArrayList<>();
+    RecyclerView recyclerView;
+    VideoFoldersAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.folders_rv);
+        showFolders();
+    }
+
+    private void showFolders() {
+        mediaFiles = fetchMedia();
+        adapter = new VideoFoldersAdapter(mediaFiles, allFolderList, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,
+                RecyclerView.VERTICAL, false));
+        adapter.notifyDataSetChanged();
+    }
+
+    public ArrayList<MediaFiles> fetchMedia(){
+        ArrayList<MediaFiles> mediaFilesArrayList = new ArrayList<>();
+        Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+
+        Cursor cursor = getContentResolver().query(uri, null,
+                null, null, null);
+        if (cursor != null && cursor.moveToNext()) {
+            do {
+                String id = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media._ID));
+                String title = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.TITLE));
+                String displayName = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME));
+                String size = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.SIZE));
+                String duration = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DURATION));
+                String path = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
+                String dateAdded = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATE_ADDED));
+                MediaFiles mediaFiles = new MediaFiles(id, title, displayName, size, duration, path,
+                        dateAdded);
+
+                int index = path.lastIndexOf("/");
+                String subString = path.substring(0, index);
+                if (!allFolderList.contains(subString)) {
+                    allFolderList.add(subString);
+                }
+                mediaFilesArrayList.add(mediaFiles);
+            } while (cursor.moveToNext());
+        }
+        return mediaFilesArrayList;
     }
 }
