ac4709895b246fb11ad0313d8e864de35478cb40
ndtthanh <tntthanh32@gmail.com>
2024-05-07 22:17:31 +0700

    Properties;search;brightness_mode;lock_unlock;fill_full_fit;sort_update

---
+++ app/src/main/java/com/prelude/preludeplayer/IconModel.java
@@ -0,0 +1,20 @@
+package com.prelude.preludeplayer;
+
+public class IconModel {
+    private int imageView;
+    private String iconTitle;
+    public IconModel(int imageView, String iconTitle){
+        this.imageView = imageView;
+        this.iconTitle = iconTitle;
+    }
+    public int getImageView(){
+        return imageView;
+    }
+    public void setImageView(int imageView){
+        this.imageView = imageView;
+    }
+    public String getIconTitle(){
+        return iconTitle;
+    }
+    public void setIconTitle(String iconTitle){ this.iconTitle = iconTitle; }
+}
